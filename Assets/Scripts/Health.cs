415e874472792e05a286779c76ae01fb180d6866
Lục Lập <boylap7@gmail.com>
2024-06-04 15:28:22 +0700

    explosion
    make effect when projectile touching player or enemy

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
-
+    [SerializeField] ParticleSystem hitEffect;
     void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
         if (damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamge());
+            PlayHitEffect();
             damageDealer.Hit();
         }
     }
@@ -22,6 +23,15 @@ public class Health : MonoBehaviour
         if (health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect()
+    {
+        if (hitEffect != null)
+        {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax );
         }
     }
 }
